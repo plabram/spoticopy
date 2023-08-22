842c53c394652a29947706b3b1dd4ea295434810
Penelope <freyalabram@gmail.com>
2023-08-22 12:32:45 +0200

    API call refactor

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,23 @@ import Steps from './sections/sections/Steps'
 import Header from './sections/Header'
 import Footer from './sections/Footer'
 import { useState, useEffect } from 'react'
+import { getVideos } from './api/call'
 
 function App() {
 
   const [videos, setVideos] = useState()
 
   useEffect(() => {
+    // const getVideos = async () => {
+    //   try {
+    //     const data = await fetch("https://64d93689e947d30a260a0107.mockapi.io/api/Videos")
+    //     const dataToJson = await data.json()
+    //     setVideos(dataToJson)
+    //   }
+    //   catch (error) { console.log("error:", error) }
+    // }
+    getVideos(setVideos)
 
-    const getVideos = async () => {
-
-      try {
-        const data = await fetch("https://64d93689e947d30a260a0107.mockapi.io/api/Videos")
-        const dataToJson = await data.json()
-        setVideos(dataToJson)
-
-      }
-      catch (error) { console.log("error:", error) }
-
-    }
-
-    getVideos()
   }, [])
 
   return (
---
+++ src/api/call.js
@@ -0,0 +1,16 @@
+
+
+
+ export const getVideos = async (videoSetter) => {
+ const SPOTI_URL = import.meta.env.VITE_APP_SPOTICOPY_URL
+//  "https://64d93689e947d30a260a0107.mockapi.io/api/Videos"
+
+      try {
+        const data = await fetch(SPOTI_URL)
+        const dataToJson = await data.json()
+        console.log("If you're API and you know it, clap your hands")
+        videoSetter(dataToJson)
+
+      }
+      catch (error) { console.log("error:", error) }
+    }
